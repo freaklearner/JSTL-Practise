e7b8b34227e0b694f304ddc94f8eea01d721239a
freaklearner <shub.shubhamthakur@gmail.com>
2018-03-22 23:52:16 +0530

    model class 'Student' created

---
+++ JSTL-forEach/src/org/com/shub/model/Student.java
@@ -0,0 +1,35 @@
+package org.com.shub.model;
+
+public class Student {
+
+	private int id;
+	private String name;
+	private String email;
+	private String adrs;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAdrs() {
+		return adrs;
+	}
+	public void setAdrs(String adrs) {
+		this.adrs = adrs;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
